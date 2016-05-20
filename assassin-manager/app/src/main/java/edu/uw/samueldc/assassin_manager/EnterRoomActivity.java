df1d7a55d7c633acb7849d157537f35b98944792
Vermilion <Vermilion@D-108-179-156-90.dhcp4.washington.edu>
2016-05-20 12:51:36 -0700

    update for new enter activities

---
+++ assassin-manager/app/src/main/java/edu/uw/samueldc/assassin_manager/EnterRoomActivity.java
@@ -0,0 +1,12 @@
+package edu.uw.samueldc.assassin_manager;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class EnterRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
