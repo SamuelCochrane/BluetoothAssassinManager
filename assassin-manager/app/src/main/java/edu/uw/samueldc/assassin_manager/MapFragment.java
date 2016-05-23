9d1d7c64f56525c1f15b47d3eee200ccb3290f77
Emile Wang <Emile@Emile-Macair.local>
2016-05-22 22:51:42 -0700

    fix a map bug

--- assassin-manager/app/src/main/java/edu/uw/samueldc/assassin_manager/MapFragment.java
+++ assassin-manager/app/src/main/java/edu/uw/samueldc/assassin_manager/MapFragment.java
@@ -7,6 +7,8 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
@@ -34,133 +36,54 @@ import android.view.ViewGroup;
 
 import java.util.ArrayList;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
-public class MapFragment extends Fragment implements OnMapReadyCallback,GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,com.google.android.gms.location.LocationListener {
+public class MapFragment extends Fragment {
 
-    private static final String TAG = "***MapFragment***";
-    private final int PERMISSION_CODE = 1;
-    private GoogleApiClient myGoogleApiClient;
-    private GoogleMap myMap;
-    private Location curLocation;
+    private GoogleMap map;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    
 
-    }
-    @Override
+    static final LatLng ME = new LatLng(47.654980, -122.307560);
+    static final LatLng PLAYER1 = new LatLng(47.654995, -122.307580);
+    static final LatLng PLAYER2 = new LatLng(47.654965, -122.307570);
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+                             Bundle savedInstanceState) {
 
+        View v = inflater.inflate(R.layout.fragment_map, null, false);
 
+        map = ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map))
+                .getMap();
 
-    // create api client
-    if (myGoogleApiClient == null) {
-        myGoogleApiClient = new GoogleApiClient.Builder(getActivity())
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-    }
-
-        // let the user locate to the current location
-        SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager()
-                .findFragmentById(R.id.map);
-        myMap = mapFragment.getMap();
-
-        // retain the fragment when activity re-creates
-        mapFragment.setRetainInstance(true);
-
-        try {
-            myMap.setMyLocationEnabled(true);
-            LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-            Criteria criteria = new Criteria();
-            String bestProvider = locationManager.getBestProvider(criteria, true);
-            Location location = locationManager.getLastKnownLocation(bestProvider);
-        } catch (SecurityException e) {
-        }
-
-        LatLng me = new LatLng(curLocation.getLatitude(),curLocation.getLongitude());
-        LatLng player1 = new LatLng(47.654995, -122.307577);
-        LatLng player2 = new LatLng(47.654998, -122.307570);
-
-        Marker marker0  = myMap.addMarker(new MarkerOptions().position(me)
+        Marker me = map.addMarker(new MarkerOptions()
+                .position(ME)
                 .title("Me")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.map_me)));
-        Marker marker1 = myMap.addMarker(new MarkerOptions().position(player1)
-                .title("Alive")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.map_alive)));
-        Marker marker2 = myMap.addMarker(new MarkerOptions()
-                .position(player2)
-                .title("Dead")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.map_dead)));
+                .icon(BitmapDescriptorFactory
+                        .fromResource(R.drawable.map_me)));
+        Marker player1 = map.addMarker(new MarkerOptions()
+                .position(PLAYER1)
+                .title("Player 1")
+                .icon(BitmapDescriptorFactory
+                        .fromResource(R.drawable.map_dead)));
+        Marker player2 = map.addMarker(new MarkerOptions()
+                .position(PLAYER2)
+                .title("Player 2")
+                .icon(BitmapDescriptorFactory
+                        .fromResource(R.drawable.map_alive)));
 
-        return inflater.inflate(R.layout.fragment_target, container, false);
-    }
+        // Move the camera instantly to myself with a zoom of 20.
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(ME, 100));
 
-    public void onStart() {
-        super.onStart();
-        myGoogleApiClient.connect();
-    }
+        // Zoom in, animating the camera.
+        map.animateCamera(CameraUpdateFactory.zoomTo(20), 2000, null);
 
-    public void onStop() {
-        super.onStop();
-        myGoogleApiClient.disconnect();
-    }
+        //...
 
-
-    @Override
-    public void onMapReady(GoogleMap map) {
-        myMap = map;
-        myMap.animateCamera(CameraUpdateFactory.zoomTo(10));
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        // build GPS request
-        LocationRequest request = new LocationRequest();
-        request.setInterval(1000);
-        request.setFastestInterval(500);
-        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-        // check permission from the user
-        int permissionCheck = ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION);
-        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
-            LocationServices.FusedLocationApi.requestLocationUpdates(myGoogleApiClient, request, this);
-        } else {
-            ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_CODE);
-        }
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-
-    //    LatLng newLocation = new LatLng(location.getLatitude(),location.getLongitude());
-        curLocation = location;
-        }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-    }
-
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permission[], int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSION_CODE:
-                // if have permission
-                if (permission.length > 0 &&
-                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    onConnected(null);
-                }
-        }
-        super.onRequestPermissionsResult(requestCode, permission, grantResults);
+        return v;
     }
 
 //    @Override
